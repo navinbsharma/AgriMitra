7c30cea30ec380afb3deb7cec8330d44c855e980
navinsharma137 <navinsharmasuprb@gmail.com>
2019-12-04 17:43:13 +0530

    ouch

--- app/src/main/java/com/example/agrimitra/QueryActivity.java
+++ app/src/main/java/com/example/agrimitra/QueryActivity.java
@@ -2,7 +2,11 @@ package com.example.agrimitra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QueryActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class QueryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query);
+
+        Button submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                Uri uri = Uri.parse("mailto:navinsharmasuprb@gmail.com");
+                intent.setData(uri);
+                intent.putExtra("subject", "my subject");
+                intent.putExtra("body", "my location");
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
