12e80f03a2074e97c8cf837fc6f5243c0c8de3c1
MGMTPG LAB <mgmtpg.lab@mitwpu.edu.in>
2019-12-04 14:59:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/agrimitra/SoilHealthActivity.java
@@ -0,0 +1,14 @@
+package com.example.agrimitra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoilHealthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soil_health);
+    }
+}
