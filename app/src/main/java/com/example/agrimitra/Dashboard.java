9c25ab173cc8654ce68916dde89b440776f913e6
navinsharma137 <navinsharmasuprb@gmail.com>
2019-12-06 16:15:44 +0530

    version1.2 DashboardHalfFunctionality

--- app/src/main/java/com/example/agrimitra/Dashboard.java
+++ app/src/main/java/com/example/agrimitra/Dashboard.java
@@ -2,7 +2,11 @@ package com.example.agrimitra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +14,51 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        ImageView query = findViewById(R.id.query);
+        ImageView weather =  findViewById(R.id.weather);
+        ImageView feedback =  findViewById(R.id.feedback);
+        ImageView soil =  findViewById(R.id.soilcard);
+        ImageView market =  findViewById(R.id.market);
+
+        query.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this , QueryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        feedback.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this , FeedbackActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        soil.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this , SoilHealthActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weather.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this , WeatherActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        market.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this , MarketActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
