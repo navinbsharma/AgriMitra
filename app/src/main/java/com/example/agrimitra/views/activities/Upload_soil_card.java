c307fe9f1a756fa502a0477b3966d21acc24d80e
jayapunjabi <jayapunjabi707@gmail.com>
2020-02-06 12:10:55 +0530

    soil card layout updated

---
+++ app/src/main/java/com/example/agrimitra/views/activities/Upload_soil_card.java
@@ -0,0 +1,16 @@
+package com.example.agrimitra.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agrimitra.R;
+
+public class Upload_soil_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_soil_card);
+    }
+}
